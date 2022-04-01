c9f744be525e3525fd494fb8335a764016d88fc5
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-04-01 15:00:37 -0700

    f

--- Myapp.py
+++ Myapp.py
@@ -23,6 +23,6 @@ import pandas as pd
 #st.markdown("*este es una viñeta*")
 
 num1 = st.slider('Elige un numero 1',0.0,100.0,25.0)
-num1 = st.slider('Elige un numero 2',0.0,100.0,25.0)
+num2 = st.slider('Elige un numero 2',0.0,100.0,25.0)
 suma= num1+num2
-st.write("la suma de",num1,"y",num2,"es",suma)
+st.write("la suma de",num1,"y",num2,"es:",suma)
