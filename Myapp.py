1166753a951f17c5aa426fae2c8f8836b4ca3597
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-04-01 14:24:12 -0700

    link 1

--- Myapp.py
+++ Myapp.py
@@ -3,7 +3,7 @@ st. title("mi pirmer app")
 #st.button("dale click")
 #st.button("otro boton")
 import pandas as pd
-df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/data.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv')
 
 st.write(df)
 
