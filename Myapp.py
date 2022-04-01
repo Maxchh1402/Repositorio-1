313bd5670d2117e28bf10d279b889bd33e261312
Maxchh1402 <102831688+Maxchh1402@users.noreply.github.com>
2022-04-01 15:00:06 -0700

    fgj

--- Myapp.py
+++ Myapp.py
@@ -2,6 +2,8 @@ import streamlit as st
 st. title("mi pirmer app")
 click=st.button("dale click")
 st.write("el valor de click es:", click)
+if click==True:
+    st.image("LOGO UACH.png")
 #st.button("otro boton")
 
 if click==True:
@@ -20,3 +22,7 @@ import pandas as pd
 #st.latex("\int_1^6")
 #st.markdown("*este es una viñeta*")
 
+num1 = st.slider('Elige un numero 1',0.0,100.0,25.0)
+num1 = st.slider('Elige un numero 2',0.0,100.0,25.0)
+suma= num1+num2
+st.write("la suma de",num1,"y",num2,"es",suma)
